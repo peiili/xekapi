162376b22e860488d5c5807676fe0cc48e0152f8
peiili <1017935195@qq.com>
2019-09-08 18:58:05 +0800

    add status

--- routes/GrabBag.js
+++ routes/GrabBag.js
@@ -5,7 +5,11 @@ const router = express.Router()
 
 router.get('/getList', (req, res) => {
   cheerioFunc.start((ress) => {
-    res.send(ress);
+    const data = {
+      success: true,
+      data: ress,
+    }
+    res.status(200).send(data);
   })
 })
 module.exports = router;
