861a048e97cd3a61e8b9b0c8b67b7937ee5f6816
peiili <1017935195@qq.com>
2019-10-14 02:01:21 +0800

    增加表单信息获取

--- routes/Active.js
+++ routes/Active.js
@@ -1,4 +1,6 @@
-const express = require('express')
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const uuidv4 = require('uuid/v4');
 const db = require('../database/connection');
 
 const router = express.Router()
@@ -27,5 +29,29 @@ router.post('/activeContent', (req, res) => {
     }
     res.status(200).send(data);
   })
+})
+
+// 临时存储验证码
+const tempCode = {};
+
+// 获取验证码
+router.post('/getAuditCode', (req, res) => {
+  const token = jwt.sign({ foo: 'bar' }, 'shhhhh');
+  let code = '';
+  code = Math.floor(Math.random() * 10000);
+  tempCode[req.body.mobilePhone] = code;
+  console.log(tempCode);
+  res.status(200).send({ token, code })
+
+})
+
+// 提交活动报名信息
+router.post('/uploadVisitorInfo', (req, res) => {
+  const data = {
+    success: true,
+    data: { ...req.body, id: uuidv4() },
+  }
+  res.status(200).send(data)
+
 })
 module.exports = router;
