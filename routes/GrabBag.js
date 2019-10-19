b7feb106791e11807466d0cf38dd05fbf8f9a389
peiili <1017935195@qq.com>
2019-10-19 22:58:48 +0800

    feat:文章列表懒加载

--- routes/GrabBag.js
+++ routes/GrabBag.js
@@ -7,14 +7,27 @@ const router = express.Router()
 router.get('/getList', (req, res) => {
 
   // req type 类型
-  const sql = 'SELECT `id`,`title`,`created_date`,`thumbnail`,`desc` FROM `qdm174930677_db`.`xek_article` WHERE type = ? ORDER BY `created_date` DESC;'
-  db.db(sql, [req.query.type], (e) => {
-    const data = {
-      success: true,
-      data: e,
-    }
-    res.status(200).send(data);
-  })
+  const sql = 'SELECT `id`,`title`,`created_date`,`thumbnail`,`desc` FROM `qdm174930677_db`.`xek_article` WHERE type = ? ORDER BY `created_date` DESC LIMIT ?,?;'
+
+  /**
+   * 参数
+   * 文章类型，
+   * 当前页数，
+   * 每页显示条数，默认10
+   */
+  const { type, page, size } = req.query;
+  try {
+    db.db(sql, [type, (page - 1) * Number(size) || 0, Number(size) || 10], (e) => {
+      const data = {
+        success: true,
+        data: e,
+      }
+      res.status(200).send(data);
+    })
+  } catch (error) {
+
+  }
+
 });
 
 // 获取文章内容
