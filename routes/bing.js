3c497bf236f71111f4508a4f948197d2105fe7fd
peiili <1017935195@qq.com>
2019-11-25 23:57:16 +0800

    接口自定义尺寸

--- routes/bing.js
+++ routes/bing.js
@@ -7,7 +7,8 @@ router.get('/randomBing', (req, res) => {
     const sql = 'SELECT * FROM xek_bing WHERE id >= ((SELECT MAX(id) FROM xek_bing) - (SELECT MIN(id) FROM xek_bing)) * RAND() + (SELECT MIN(id) FROM xek_bing) LIMIT 1 ;'
     db.db(sql, [], (resp) => {
         console.log(resp[0].name);
-        res.redirect(302, `https://www.dlsjf.top/${resp[0].name}`);
+        console.log(req.query);
+        res.redirect(302, `https://www.dlsjf.top/${resp[0].name}?imageView2/2/w/${req.query.w}`);
     })
 })
 module.exports = router;
