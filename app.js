c5b6857177f61d72476653a5309e3b6635f52740
peiili <1017935195@qq.com>
2020-03-15 19:25:24 +0800

    增加prettier格式化，修改随机壁纸sql

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const morgan = require('morgan')
-const express = require('express')
-const process = require('process')
+const morgan = require('morgan');
+const express = require('express');
+const process = require('process');
 const bodyParser = require('body-parser');
 const getDomDate = require('./src/cheerio');
 
-const app = express()
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 if (process.env.NODE_ENV) {
-  app.use(morgan('dev'))
+  app.use(morgan('dev'));
 } else {
-  app.use(morgan('short'))
+  app.use(morgan('short'));
 }
 
 const grabbag = require('./routes/GrabBag');
@@ -23,7 +23,7 @@ app.use('/api/grabbag', grabbag);
 app.use('/api/active', active);
 app.use('/api/bing', bing);
 app.use('/api/user', user);
-const port = 5166
+const port = 5166;
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
--- routes/bing.js
+++ routes/bing.js
@@ -1,14 +1,15 @@
-const express = require('express')
+const express = require('express');
 const db = require('../database/connection');
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/randomBing', (req, res) => {
-    const sql = 'SELECT * FROM xek_bing WHERE id >= ((SELECT MAX(id) FROM xek_bing) - (SELECT MIN(id) FROM xek_bing)) * RAND() + (SELECT MIN(id) FROM xek_bing) LIMIT 1 ;'
-    db.db(sql, [], (resp) => {
-        console.log(resp[0].name);
-        console.log(req.query);
-        res.redirect(302, `https://www.dlsjf.top/${resp[0].name}?imageView2/2/w/${req.query.w}`);
-    })
-})
+  const sql = 'SELECT * FROM `xek_bing` ORDER BY RAND() LIMIT 1;';
+  console.log(sql);
+  db.db(sql, [], resp => {
+    console.log(resp[0].name);
+    console.log(req.query);
+    res.redirect(302, `https://www.dlsjf.top/${resp[0].name}?imageView2/2/w/${req.query.w}`);
+  });
+});
 module.exports = router;
