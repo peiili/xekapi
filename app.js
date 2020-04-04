1de5909c58c8e881fb206d6e8bd0d839dddfcd97
peiili <1017935195@qq.com>
2020-04-05 00:54:49 +0800

    添加工程格式化

--- app.js
+++ app.js
@@ -1,29 +1,49 @@
-const morgan = require('morgan')
-const express = require('express')
-const process = require('process')
+const morgan = require('morgan');
+const express = require('express');
+const process = require('process');
 const bodyParser = require('body-parser');
 const getDomDate = require('./src/cheerio');
 
-const app = express()
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 if (process.env.NODE_ENV) {
-  app.use(morgan('dev'))
+  app.use(morgan('dev'));
 } else {
-  app.use(morgan('short'))
+  app.use(morgan('short'));
 }
+
+// app.all('*', (req, res, next) => {
+//   const { origin, Origin, referer, Referer } = req.headers;
+//   const allowOrigin = origin || Origin || referer || Referer || '*';
+//   res.header('Access-Control-Allow-Origin', allowOrigin);
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Content-Type, Authorization, X-Requested-With'
+//   );
+//   res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+//   res.header('Access-Control-Allow-Credentials', true); // 可以带cookies
+//   res.header('X-Powered-By', 'Express');
+//   if (req.method == 'OPTIONS') {
+//     res.sendStatus(200);
+//   } else {
+//     next();
+//   }
+// });
 
 const grabbag = require('./routes/GrabBag');
 const active = require('./routes/Active');
 const bing = require('./routes/bing');
 const user = require('./routes/user');
+const attachment = require('./routes/attachment');
 
 getDomDate.start();
 app.use('/api/grabbag', grabbag);
 app.use('/api/active', active);
 app.use('/api/bing', bing);
 app.use('/api/user', user);
-const port = 5166
+app.use('/api/attachment', attachment);
+const port = 5166;
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
