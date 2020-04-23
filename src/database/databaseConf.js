d9e60b005228b0e7e140470d9beabe2a91a56145
peiili <1017935195@qq.com>
2020-04-24 00:37:22 +0800

    修改环境变量

--- src/database/databaseConf.js
+++ src/database/databaseConf.js
@@ -1,18 +1,18 @@
 const production = {
   host: '127.0.0.1',
-  password: 'ZZZ123456',
+  password: process.env.DB_PASSWORD,
   user: 'root',
-  database: 'xek',
+  database: 'xek'
 };
 const devs = {
   host: '47.105.113.47',
-  password: 'ZZZ123456',
+  password: process.env.DB_PASSWORD,
   user: 'root',
-  database: 'xek_test',
+  database: 'xek_test'
 };
 let env = '';
 env = process.env.NODE_ENV;
 console.log(process.env.NODE_ENV);
 module.exports = {
-  databaseConfig: env.trim() === 'dev' ? devs : production,
+  databaseConfig: env.trim() === 'development' ? devs : production
 };
