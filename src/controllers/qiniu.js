77e05208dd3181d4c6288884fab9062b3f771120
peiili <1017935195@qq.com>
2022-03-13 23:47:25 +0800

    feat:上传附件

--- src/controllers/qiniu.js
+++ src/controllers/qiniu.js
@@ -5,12 +5,12 @@ const accesskey = 'sKinjQ5quCjitqHIAdhSNVjMpW7fB0aud5bVdjuR';
 const SecretKey = 'YxdUUrWMa2bl7g60nFOGlvUAy42x4OvEOoJxmYf1';
 
 // 空间对应的机房
-config.zone = qiniu.zone.Zone_z2;
+config.zone = qiniu.zone.Zone_z0;
 const formUploader = new qiniu.form_up.FormUploader(config);
 const putExtra = new qiniu.form_up.PutExtra();
 
 const options = {
-  scope: 'statich5',
+  scope: 'xek',
   returnBody: '{"key":"$(key)","hash":"$(etag)","fsize":$(fsize),"bucket":"$(bucket)","name":"$(x:name)"}',
   callbackBodyType: 'application/json'
 };
@@ -18,23 +18,33 @@ const putPolicy = new qiniu.rs.PutPolicy(options);
 const mac = new qiniu.auth.digest.Mac(accesskey, SecretKey);
 const Token = putPolicy.uploadToken(mac);
 
-function putStreams(key, localFile) {
+const  putStreams =function(key, localFile) {
   formUploader.putFile(Token, key, localFile, putExtra, (respErr, respBody, respInfo) => {
     if (respErr) {
       throw respErr;
     }
     if (respInfo.statusCode === 200) {
       console.log(respBody);
-
-      // const sql = 'insert into `xek_bing` (`id`,`name`,`describe`,`bash`,`create_date`) values (?,?,?,?,NOW());';
-      // const value = [new Date().getTime(), respBody.key, JSON.stringify(desc), respBody.hash];
-      // uploadImg(sql, value, resp => {
-      //   console.log(resp);
-      // });
     } else {
       console.log(respInfo.statusCode);
       console.log(respBody);
     }
   });
 }
-module.exports = putStreams;
+const formStreams= function(key,stream,cb){
+  var formUploader = new qiniu.form_up.FormUploader(config);
+  var putExtra = new qiniu.form_up.PutExtra();
+  var readableStream = stream; // 可读的流
+  formUploader.putStream(Token, key, readableStream, putExtra, function (respErr,
+    respBody, respInfo) {
+    if (respErr) {
+      throw respErr;
+    }
+    cb(respInfo,respBody)
+  });
+}
+// exports.formStreams = formStreams;
+module.exports = {
+  putStreams,
+  formStreams
+};
--- src/routes/attachment.js
+++ src/routes/attachment.js
@@ -1,16 +1,19 @@
-// 附件上传， from
+// 附件上传， form
 const express = require('express');
+const db = require('../database/connection');
 const multer = require('multer');
-
-const putStream = require('../controllers/qiniu');
+const fs = require('fs')
+const {putStream,formStreams} = require('../controllers/qiniu');
 
 const router = express.Router();
 
+const {response,errorMsg} = require('../controllers/reponsecontroller')
+
 const storage = multer.diskStorage({
-  destination(req, file, cb) {
+  destination (req, file, cb) {
     cb(null, '/tmp/my-img');
   },
-  filename(req, file, cb) {
+  filename (req, file, cb) {
     cb(null, file.originalname);
   }
 });
@@ -46,4 +49,26 @@ router.post('/', upload.single('image'), (req, res) => {
   putStream(key, localFile);
   res.send();
 });
+router.post('/uploader', upload.single('file'), (req, res) => {
+  const key = `typeset/${req.file.originalname}`;
+  const localFile = `/tmp/my-img/${req.file.originalname}`;
+  const stream = fs.createReadStream(localFile)
+  formStreams(key,stream,((respInfo,respBody)=>{
+    if (respInfo.statusCode == 200) {
+      // console.log(respBody)
+      const sql = 'insert into `xek_attachment` (`hash`,`path`,`source`,`create_date`) values (?,?,?,NOW());';
+      const value = [respBody.hash,key, req.body.source];
+      db.db(sql, value, resp => {
+        res.status(200).send(response({
+          data:key
+        }))
+      });
+    } else {
+      console.log(respInfo.statusCode);
+      console.log(respBody);
+    }
+
+
+  }))
+})
 module.exports = router;
