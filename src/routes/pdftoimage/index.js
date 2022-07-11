94c0f372025e83f8768b9ec5b497c99236744d91
燕红立 <yanhongli@369exhibition.com>
2022-07-11 16:50:05 +0800

    feat:pdf转图片

--- src/routes/pdftoimage/index.js
+++ src/routes/pdftoimage/index.js
@@ -2,8 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const { exec } = require('child_process');
+const moment = require('moment')
 const router = express.Router();
 const multer = require('multer');
+const db = require('../../database/connection');
 const {putStreams} = require('../../controllers/qiniu');
 const {response,errorMsg} = require('../../controllers/reponsecontroller')
 
@@ -27,8 +29,15 @@ router.post('/uploader',upload.single('file'), (req,res) => {
     if(!hasDir){
         fs.mkdirSync(outBasePath+outpath)
     }
-    fs.readFileSync(path.join('/tmp/my-pdf',req.file.originalname))
-
+    // fs.readFileSync(path.join('/tmp/my-pdf',req.file.originalname))
+    var key = `pdf/${moment().format('YYYYMMDDhhmmss')}-${(Math.random()*100000).toFixed(0)}${name}`
+    putStreams(key,path.join('/tmp/my-pdf',req.file.originalname),(respInfo,respBody)=>{
+        const sql = 'insert into `xek_attachment` (`hash`,`path`,`type`,`source`,`create_date`) values (?,?,2,?,NOW());';
+        const value = [respBody.hash,key, req.body.source];
+        db.db(sql, value, resp => {
+            console.log(resp.insertId)
+        });
+    })
     // python main.py 需要被转换的文件  转换后的问卷存放位置 文件夹名称
     exec(`python main.py ${path.join('/tmp/my-pdf',name)} ${path.join(outBasePath,outpath)} ${outpath}`, (err, stdout, stderr) => {
         if (err) {
@@ -37,9 +46,13 @@ router.post('/uploader',upload.single('file'), (req,res) => {
         }
         let files = []
         files = fs.readdirSync(path.join(outBasePath,outpath))
-        res.status(200).send(response(files.map(e=>{
-            return  'img/'+encodeURIComponent(outpath+'/'+e) 
-        })))
+        res.status(200).send(response({
+            id:encodeURIComponent(outpath),
+            images:files.map(e=>{
+                return  'img/'+encodeURIComponent(outpath+'/'+e) 
+            })
+        }
+        ))
     })
     // 异步操作
     // res.redirect(`http://127.0.0.1:${port}/result.html?id=${outpath}`)
