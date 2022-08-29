c6f401f983c09df4921798fd645bbc7a68c7076c
peiili <hongliyan1024@gmail.com>
2022-08-29 21:42:25 +0800

    添加网站管理接口

---
+++ src/routes/website/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const db = require('../../database/connection');
+const router = express.Router();
+const {response,errorMsg} = require('../../controllers/reponsecontroller');
+
+router.get('/',function(req,res){
+  var sql = 'select * from xek_website where id=?'
+  db.db(sql,[2],function(resp){
+    res.status(200).send(response(resp[0]));
+  })
+})
+router.put('/',function(req,res){
+  const { title='',description='',host='',keywords='',copyright='' } = req.body;
+  var sql = 'update xek_website set title=?,description=?,host=?,keywords=?,copyright=? where id=2'
+  db.db(sql,[title,description,host,keywords,copyright],function(resp){
+    res.status(200).send({
+      title,
+      description,
+      host,
+      keywords,
+      copyright
+    });
+  })
+})
+
+module.exports=router
+
