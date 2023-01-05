2aafe0c66f93f71e774885f05987c1b8bf81e5f6
yanhongli <1017935195@qq.com>
2023-01-06 01:26:02 +0800

    fix: 优化缺少参数提示

--- src/routes/log/index.js
+++ src/routes/log/index.js
@@ -8,6 +8,9 @@ router.get('/',(req,res)=>{
   var tag = req.query.tag
   var user_data = req.query.userData||''
   var user_client = JSON.stringify(req.rawHeaders)
+  if(!tag){
+    res.status(400).send(errorMsg('tag is required'))
+  }
   var sql = `insert into custom_logs (ip,host,tag,create_time,user_data,user_client) values (?,?,?,NOW(),?,?)`
   db.query(sql,[ip,host,tag,user_data,user_client],()=>{
     res.status(200).send(response({}))
