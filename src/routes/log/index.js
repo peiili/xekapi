c21d0b69dd20260e5281d3ab8bcc9169728b196a
yanhongli <1017935195@qq.com>
2023-01-06 09:50:58 +0800

    fix: 通过nginx转发请求头获取真实IP

--- src/routes/log/index.js
+++ src/routes/log/index.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const db = require('../../database/connection');
 const {response,errorMsg} = require('../../controllers/reponsecontroller');
 router.get('/',(req,res)=>{
-  var ip = req.ip
+  // var ip = req.ip
   var host = req.hostname
   var tag = req.query.tag
   var user_data = req.query.userData||''
   var user_client = JSON.stringify(req.rawHeaders)
+  // 使用nginx 代理时无法获取真实ip，需要在nginx中自定义请求头进行转发真实ip
+  var ip = req.headers['X-real-ip']
   if(!tag){
     res.status(400).send(errorMsg('tag is required'))
     return
