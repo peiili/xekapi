3c9b38c6ba21bb430d4e4768305eaf5289f1124c
hongli <yan@honglis-Mac-mini.local>
2023-06-24 15:54:50 +0800

    feat: record ip address

--- src/routes/log/index.js
+++ src/routes/log/index.js
@@ -1,7 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request')
 const db = require('../../database/connection');
 const {response,errorMsg} = require('../../controllers/reponsecontroller');
+
+var store = {} 
+function getIpAddress(ip,cb){
+  var url = `https://opendata.baidu.com/api.php?query=${ip}&co=&resource_id=6006&oe=utf8`
+  request.get(url,(err,res,body)=>{
+    if(err){
+      throw err
+    }else{
+      var _body = JSON.parse(body)
+      if(_body.status==='0'){
+        cb(_body.data[0].location)
+      }else{
+        cb('unknown')
+      }
+    }
+  })
+}
+/**
+ * 写入数据库
+ * @param {Object} db 
+ * @param {Array} data 
+ */
+function toDb(db,data,res){
+  var sql = `insert into custom_logs (ip,host,tag,create_time,user_data,user_client,location) values (?,?,?,NOW(),?,?,?)`
+  db.query(sql,data,()=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','*');
+    res.status(200).send(response({}))
+  })
+}
 router.get('/',(req,res)=>{
   // var ip = req.ip
   var host = req.headers['referer']
@@ -16,12 +47,20 @@ router.get('/',(req,res)=>{
     res.status(400).send(errorMsg('tag is required'))
     return
   }
-  var sql = `insert into custom_logs (ip,host,tag,create_time,user_data,user_client) values (?,?,?,NOW(),?,?)`
-  db.query(sql,[ip,host,tag,user_data,user_client],()=>{
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','*');
-    res.status(200).send(response({}))
-  })
+  var location = ''
+  if(store[ip]){
+    location = store[ip]
+    var data = [ip,host,tag,user_data,user_client,location]
+    toDb(db,data,res)
+  }else{
+    getIpAddress(ip,function(dst){
+      store[ip] = dst
+      location = dst
+      var data = [ip,host,tag,user_data,user_client,location]
+      toDb(db,data,res)
+    })
+  }
+ 
 })
 router.get('/tongji',(req,res)=>{
   res.setHeader('Access-Control-Allow-Origin','*');
