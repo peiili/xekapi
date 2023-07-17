62667da9416fab008363a2ee1d8b577ff214cc7f
hongli <yan@honglis-Mac-mini.local>
2023-07-17 23:20:48 +0800

    feat: 渲染文章

--- src/routes/views/index.js
+++ src/routes/views/index.js
@@ -47,7 +47,12 @@ router.get('/article',(req,res)=>{
 })
 
 router.get('/article/:id',(req,res)=>{
-    res.render('articleDetails',{current:'article'})
+  const countSql = 'SELECT title,content FROM `xek_article` WHERE id=?'
+  db.db(countSql,[req.params.id],data=>{
+    var title = data[0].title
+    var content = data[0].content
+    res.render('articleDetails',{current:'article',title:title,content:content})
+  })
   })
   
 router.get('/other',(req,res)=>{
