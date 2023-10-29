ed8a3ada8e4c5c381be09726af36f297fccf6b90
hongli <yan@honglis-Mac-mini.local>
2023-10-29 11:55:18 +0800

    fix order

--- src/routes/views/home_route.js
+++ src/routes/views/home_route.js
@@ -1,6 +1,6 @@
 module.exports  = function(req, res){
   var db = req.db
-  var sql = `select id, DATE_FORMAT(createdate, '%Y-%m-%d') createdate,TIME_FORMAT(createtime, '%H:%i:%s') createtime from news`
+  var sql = `select id, DATE_FORMAT(createdate, '%m月%d日') datetime, DATE_FORMAT(createdate, '%Y-%m-%d') createdate,TIME_FORMAT(createtime, '%H:%i:%s') createtime from news order by createdate desc`
   db.query(sql, [], function(result){
     console.log(result);
     res.render('home',{current:'home',list:result})
